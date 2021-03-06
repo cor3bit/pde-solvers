d623130cf7d28ba3d105baf562d8f8a50170c89d
core_bit <nkorbit@outlook.com>
2021-03-06 22:16:23 +0100

    Adds a DeepXDE 2D heat eqn example.

--- src/dl_solver_heat.py
+++ src/dl_solver_heat.py
@@ -3,45 +3,59 @@ import deepxde as dde
 from deepxde.backend import tf
 
 
-def pde(x, y):
-    dy_t = dde.grad.jacobian(y, x, i=0, j=1)
+def pde(x, u):
+    du_t = dde.grad.jacobian(u, x, i=0, j=0)
 
-    dy_xx = dde.grad.hessian(y, x, i=0, j=0)
+    du_xx = dde.grad.hessian(u, x, i=1, j=1)
 
-    return (
-            dy_t
-            - dy_xx
-            + tf.exp(-x[:, 1:])
-            * (tf.sin(np.pi * x[:, 0:1]) - np.pi ** 2 * tf.sin(np.pi * x[:, 0:1]))
-    )
+    du_yy = dde.grad.hessian(u, x, i=2, j=2)
+
+    return du_t - du_xx - du_yy + 6.8
 
 
 def func(x):
-    return np.sin(np.pi * x[:, 0:1]) * np.exp(-x[:, 1:])
+    # 1 + x^2 + alpha*y^2 + beta*t
+    # alpha = 3
+    # beta = 1.2
+    return 1. + x[:, 1:2] * x[:, 1:2] + 3 * x[:, 2:] * x[:, 2:] + 1.2 * x[:, 0:1]
 
 
 def main():
-    geom = dde.geometry.Interval(-1, 1)
-    timedomain = dde.geometry.TimeDomain(0, 1)
+    geom = dde.geometry.Rectangle(xmin=[0, 0], xmax=[1, 1])
+
+    timedomain = dde.geometry.TimeDomain(0, 2)
+
     geomtime = dde.geometry.GeometryXTime(geom, timedomain)
 
+    bc = dde.DirichletBC(geomtime, func, lambda _, on_boundary: on_boundary)
+
+    ic = dde.IC(geomtime, func, lambda _, on_initial: on_initial)
+
     data = dde.data.TimePDE(
-        geomtime, pde, [], num_domain=40, solution=func, num_test=10000,
+        geomtime,
+        pde,
+        ic_bcs=[ic, bc],
+        num_domain=400,
+        num_boundary=200,
+        num_initial=100,
+        num_test=10000,
+        solution=func,
     )
 
-    layer_size = [2] + [32] * 3 + [1]
-    activation = "tanh"
-    initializer = "Glorot uniform"
+    # inputs (2D Heat Eqn) - t, x, y
+    # output (solution) - u(t, x, y)
+    layer_size = [3] + [32] + [64] + [256] + [64] + [32] + [1]
+
+    activation = 'tanh'
+
+    initializer = 'Glorot uniform'
 
     net = dde.maps.FNN(layer_size, activation, initializer)
 
-    net.apply_output_transform(
-        lambda x, y: x[:, 1:2] * (1 - x[:, 0:1] ** 2) * y + tf.sin(np.pi * x[:, 0:1])
-    )
-
     model = dde.Model(data, net)
 
-    model.compile("adam", lr=0.001, metrics=["l2 relative error"])
+    model.compile('adam', lr=0.001, metrics=['l2 relative error'])
+
     losshistory, train_state = model.train(epochs=10000)
 
     dde.saveplot(losshistory, train_state, issave=True, isplot=True)
